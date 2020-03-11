d9dc3897dc865bfc86128ec73535b42a4ec83eb5
山本卓実 <yamamototakumi@koumotoukeinoMacBook-Pro.local>
2020-03-11 11:13:20 +0900

    thread per message

---
+++ src/m7_thread_per_message/Helper.java
@@ -0,0 +1,20 @@
+package m7_thread_per_message;
+
+public class Helper {
+    void handle(int count, char c) {
+        System.out.println("        handle(" + count + ", " + c + ") BEGIN");
+        for (int i = 0; i < count; i++) {
+            slowly();
+            System.out.println(c);
+        }
+        System.out.println("");
+        System.out.println("        handle(" + count + ", " + c + ") END");
+    }
+
+    private void slowly() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+        }
+    }
+}
